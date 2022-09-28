b4a300e0eb303ffbd50c49c3312718d607f5c9ab
Orange Moose <erinkodara.verslui@gmail.com>
2022-09-28 14:30:22 +0300

    Initial commit

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")] // default route for this controller
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult
+                (
+                new List<object>()
+                    {
+                        new {id = 1, name = "Vilnius" },
+                        new {id = 2, name = "Kaunas"}
+                    }
+                );
+        }
+
+        
+    }
+}
